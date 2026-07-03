14789ebe7e5a53f0c687d227f698ad09fae7d576
shravanpradeep1234-cloud <shravanpradeep1234@gmail.com>
2026-07-03 10:19:14 +0530

    Update appp.py

--- appp.py
+++ appp.py
@@ -3,6 +3,7 @@ import streamlit as st
 name- st.text_input("enter your name")
 
 st.title("take the input name")
-if st.bottom("submit Name"):
+
+if st.bottom("submit"):
    sr.write(f"print the name : (name)")
   
